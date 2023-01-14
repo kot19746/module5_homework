c7ae91350919fd56e141cbe8356ea209ad1bb540
Fargrab <78262823+Fargrab@users.noreply.github.com>
2023-01-14 13:26:03 +0300

    update script6

--- Script6.js
+++ Script6.js
@@ -3,13 +3,14 @@
 Дан массив. Проверить, одинаковые ли элементы в массиве и вывести результат true или false в консоль.
 Речь идёт не о двух рядом стоящих одинаковых элементах, а обо всех. Проверить, все ли элементы в массиве одинаковые.*/
 
-function verifArr(arr, i = 0) {
-  if (arr.length < 2 || i == arr.length-1) {
-    return true;
-  }
-  return typeof arr[i] !== typeof arr[i+1] 
-    ? false 
-    : verifArr(arr, ++i)
+function verifArr(arr) {
+    let result = true;
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if(arr[i] !== arr[i + 1]) {
+            result = false;
+        }
+    }
+    return result;
 }
 console.log(verifArr([10, 15, 20, 25, 30]))
 console.log(verifArr([5, 'abc', null, 7, 18]))
